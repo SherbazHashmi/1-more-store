b6393fe927458c7f42d2b910880bfe648fcba66d
Sherbaz <sherbaz.hashmi@gmail.com>
2018-02-28 19:33:40 +1100

    Added Cart Button Skeleton

---
+++ 1more-store/View/Navigation Bar/CartButton.swift
@@ -0,0 +1,13 @@
+//
+//  CartButton.swift
+//  1more-store
+//
+//  Created by Sherbaz Hashmi on 28/2/18.
+//  Copyright © 2018 Sherbaz Hashmi. All rights reserved.
+//
+
+import UIKit
+
+class CartButton: UIBarButtonItem {
+
+}
